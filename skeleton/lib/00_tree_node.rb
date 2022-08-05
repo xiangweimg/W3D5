e09ecb1fbf5fef0e8297f5f1452c80d98b35e5fd
Xiangwei Liu <xiangweimg@gmail.com>
2022-08-05 13:37:14 -0400

    ivy1

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -1,3 +1,26 @@
 class PolyTreeNode
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+   attr_reader :parent, :children, :value
+
+    def parent=(node)
+        if !@parent.nil?
+            @parent.children.delete(self)
+        end
+        @parent = node
+        if !@parent.children.include?(self)
+            @parent.children << self
+        end
+    end
+
+    def add_child(child_node)
+       child_node.parent=(self)
+    end
+
+    
 
 end
