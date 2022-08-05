52c91a0fc6fbccf5d5bf9b7eb83d817bcec8d1b7
acoop68@gmail.com <acoop68@gmail.com>
2022-08-05 13:57:55 -0400

    complete remove child

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -8,17 +8,32 @@ class PolyTreeNode
    attr_reader :parent, :children, :value
 
     def parent=(node)
+        return @parent = nil if node == nil
         if !@parent.nil?
             @parent.children.delete(self)
+            @parent = node
+            if !@parent.children.include?(self)
+                @parent.children << self
+            end
+        else
+            @parent = node
+            @parent.children << self 
         end
-        @parent = node
-        if !@parent.children.include?(self)
-            @parent.children << self
-        end
+
     end
 
     def add_child(child_node)
        child_node.parent=(self)
+    end
+
+    def remove_child(child_node)
+        # if child_node.nil?
+        #     raise "error"
+        # end
+        if child_node.parent == nil
+            raise "not a child"
+        end
+        child_node.parent = nil
     end
 
     
